1174d8642b2d1a86daf184533e55fc9bdef92baa
Jeff <medders.jeffrey@gmail.com>
2016-08-12 12:26:52 -0600

    flexbox for webpage name

--- public/javascripts/controllers/controllers.js
+++ public/javascripts/controllers/controllers.js
@@ -1,3 +1,4 @@
 app.controller('capstoneController', ['$scope', 'capstoneService', '$http', function($scope, capstoneService, $http){
   $scope.view = {};
+  $scope.view.whatsmynameagain = "hey kid"
 }])
