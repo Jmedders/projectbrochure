ac0408f86f3e0c143912dc5a1164a27712cb3a47
Jeff <medders.jeffrey@gmail.com>
2016-08-12 13:08:08 -0600

    heroku deployment

--- public/javascripts/controllers/controllers.js
+++ public/javascripts/controllers/controllers.js
@@ -1,4 +1,3 @@
 app.controller('capstoneController', ['$scope', 'capstoneService', '$http', function($scope, capstoneService, $http){
   $scope.view = {};
-  $scope.view.whatsmynameagain = "hey kid"
 }])
