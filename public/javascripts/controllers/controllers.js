5ded39f58d569452294ebfbbbb1fb8f2206465e0
Jeff <medders.jeffrey@gmail.com>
2016-08-12 14:33:31 -0600

    styling, about page functionality

--- public/javascripts/controllers/controllers.js
+++ public/javascripts/controllers/controllers.js
@@ -1,3 +1,15 @@
 app.controller('capstoneController', ['$scope', 'capstoneService', '$http', function($scope, capstoneService, $http){
   $scope.view = {};
+  $scope.view.showPlans = false;
+  $scope.view.showMore = false;
+  $scope.view.showRest = false;
+  $scope.view.change = function(){
+    $scope.view.showPlans = true;
+  };
+  $scope.view.changeShowMore = function(){
+    $scope.view.showMore = true;
+  };
+  $scope.view.changeShowRest = function(){
+    $scope.view.showRest = true;
+  };
 }])
